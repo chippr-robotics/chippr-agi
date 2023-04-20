46a8f737afef47b575b5c95e968fd0b6b1a40719
Cody Burns <cody.w.burns@gmail.com>
2023-04-20 01:02:28 +0000

    rm test.js use runme in examples

--- test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const { ChipprAGI } = require('./index.js');
-
-const OBJECTIVE = process.env.OBJECTIVE;
-
-var bot = new ChipprAGI(OBJECTIVE);
-
-module.exports = { bot };
