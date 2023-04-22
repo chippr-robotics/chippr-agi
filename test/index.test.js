42d50d246035477b0ea733cefdea2581b220d3a6
Cody Burns <cody.w.burns@gmail.com>
2023-04-22 21:48:51 +0000

    not working , not not working

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+//set the environmental variables
+//(todo) have a test env config
+//require('dotenv').config();
+import {expect} from 'chai';
+//test core functions
+//require('./core');
+describe('core Testing', () => {});
+//tests for systems
+//require('./systems');
+
+//test for components
+//require('./components');
