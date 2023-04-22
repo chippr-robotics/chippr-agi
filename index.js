1d4767a8a9694ec7b1dbb7a338e6e757dc8870be
Cody Burns <cody.w.burns@gmail.com>
2023-04-22 14:55:49 +0000

    updating to configurable index for npm push.

--- index.js
+++ index.js
@@ -6,39 +6,36 @@ import 'vm';
 import { CHIPPRAGI } from './core/ChipprAGI.js';
 import ('./systems/active/CoreSystemLoader.mjs');
 
-var events = [];
-console.clear();
 
-/// show stats if bored
-function dashboard(){
-    setInterval(() => {
-        console.clear();
-        console.log('|--- stats --|');
-        console.log(`Entities: ${JSON.stringify(Object.keys(CHIPPRAGI.entities).length)}`);   
-        console.log(`Components: ${JSON.stringify(CHIPPRAGI.components)}`);   
-        console.log(`Systems: ${JSON.stringify(CHIPPRAGI.systems)}`); 
-        console.log(`core loader: ${JSON.stringify(CHIPPRAGI.systems['CoreSystemLoader'])}`)
-        console.log(`events: `);
-        events.forEach( async msg => {
-            console.log(msg);
-        }); 
-    }, 1000);
-}
+export const CHIPPRAGI = new CHIPPRAGI({
+    //will try process.env then apply defaults
+    //defaults will be applied if not found in process.env
+    //defaults shopuld make the mvp system runable
+    //core systems
+    TESTING : process.env.CHIPPRAGI_TESTING || false,
+    CORE:{
+        SWARM_MODE: process.env.CHIPPRAGI_SWARM_MODE || true,
+        MSG_BUS : process.env.CHIPPRAGI_MSG_BUS || 'local',
+        WATCH : process.env.CHIPPRAGI_WATCH || false,
+        DASHBOARD : process.env.CHIPPRAGI_DASHBOARD || false,
+    },
+    VECTORDB:{  
+        VECTORDB_TYPE: process.env.CHIPPRAGI_VECTORDB_TYPE || 'local',
+        VECTORDB_HOST: process.env.CHIPPRAGI_REDIS_HOST || 'redis://localhost',
+        VECTORDB_PORT: process.env.CHIPPRAGI_REDIS_PORT || '6965',    
+    },
 
-dashboard();
-CHIPPRAGI.on('*', (data) => {events.push(JSON.stringify(data))});
+    LANGUAGE_MODEL:{
+        LANGUAGE_MODEL_API_KEY: process.env.CHIPPRAGI_LANGUAGE_MODEL_API_KEY || null,
+        LANGUAGE_MODEL_API_URL: process.env.CHIPPRAGI_LANGUAGE_MODEL_API_URL || null,
+        LANGUAGE_MODEL_MODEL_ID: process.env.CHIPPRAGI_LANGUAGE_MODEL_MODEL_ID || 'openai',
+        LANGUAGE_MODEL_MODEL_NAME: process.env.CHIPPRAGI_LANGUAGE_MODEL_MODEL_NAME || 'text-davinvi-003',
+        LANGUAGE_MODEL_DEFAULT_TEMP: process.env.CHIPPRAGI_LANGUAGE_MODEL_DEFAULT_TEMP || 0.5,
+        LANGUAGE_MODEL_DEFAULT_MAX_TOKENS: process.env.CHIPPRAGI_LANGUAGE_MODEL_DEFAULT_MAX || 500,
+        LANGUAGE_MODEL_DEFAULT_MATCH_LENGTH: process.env.CHIPPRAGI_LANGUAGE_MODEL_DEFAULT_MATCH_LENGTH || 5,
+    },
+    MESSAGE_BUS:{
+        MESSAGE_BUS_TYPE: process.env.CHIPPRAGI_MESSAGE_BUS_TYPE || 'local',
+    },
+});
 
-setTimeout(()=>{
-    //emit an objective
-    CHIPPRAGI.systems['CoreSystemLoader'].init(CHIPPRAGI.eventEmitter);
-}, 1000);
-
-
-setTimeout(()=>{
-    //emit an objective
-    console.log('sending objective');
-    CHIPPRAGI.emit('createObjective', { objectiveDescription: process.env.OBJECTIVE || "Test the automation system!"});
-}, 20000);
-
-
-export { CHIPPRAGI }
