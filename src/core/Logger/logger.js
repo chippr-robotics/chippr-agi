a8574ebb4040ba153ecc774d3b0612ccfafd7043
Cody Burns <cody.w.burns@gmail.com>
2023-04-28 14:14:06 +0000

    add env to index and example

--- src/core/Logger/logger.js
+++ src/core/Logger/logger.js
@@ -5,21 +5,39 @@ export class Logger {
     constructor(chipprConfig) {
       this.logger =  this.createLogger(chipprConfig.LOG.LOG_LEVEL);
     }
-    createLogger(level) {
+    createLogger(config) {
+        let consoleLogger;
+        let debugLogger;
+        //log to console if enabled
+        if(config.LOG.LOG_CONSOLE) {
+            consoleLogger = new (winston.transports.Console)({
+                level: config.LOG.LOG_LEVEL
+            });
+        } else {
+            consoleLogger = null;
+        }
+        if(config.LOG.DEBUG) {
+            debugLogger = new (winston.transports.File)({
+                name: 'debug-logs',
+                filename: path.resolve(__dirname, '../logs/debug.log'),
+                level: 'debug'
+            });
+        } else {
+            debugLogger = null;
+        }
         winston.createLogger({
             format: winston.format.json(),
             transports: [
-                new (winston.transports.Console)({
-                    level:chipprConfig.LOG.LOG_LEVEL
-                }),
+                consoleLogger,
+                debugLogger,
                 new (winston.transports.File)({
-                    name: 'info-file',
+                    name: 'info-logs',
                     filename: path.resolve(__dirname, '../logs/info.log'),
                     level: 'info'
                 }),
                 new (winston.transports.File)({
-                    name: 'error-file',
-                    filename: path.resolve(__dirname, '../logs/error.log'),
+                    name: 'error-logs',
+                    filename: path.resolve(__dirname, '../logs/errors.log'),
                     level: 'error'
                 }),
             ]
