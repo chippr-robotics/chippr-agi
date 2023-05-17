dbfebda4ed18784236fa6f581717a3b671657d68
Cody Burns <cody.w.burns@gmail.com>
2023-05-17 02:16:10 +0000

    e2e smoke test

--- test/core/langModel.test.js
+++ test/core/langModel.test.js
@@ -1,6 +1,8 @@
 import  {expect} from  "chai";
 import { OpenAIApi } from '../../src/core/LangModel/openai.js';
 import {completionTest} from './testData.js';
+import * as dotenv from "dotenv";
+
 /*{
     prompt : string latest prompt
     convo : {
@@ -10,21 +12,23 @@ import {completionTest} from './testData.js';
     }
 } 
 */
+var testConfig;
 //tests the core system class
 describe("Langmodel: openai testing", function() {
     //setup the system for testing
     before(() => {
-        var testConfig = {
+        dotenv.config({ path: '../.env' });
+         testConfig = {
             LANGUAGE_MODEL: {
-                LANGUAGE_MODEL_API_KEY : process.env.LANGUAGE_MODEL.LANGUAGE_MODEL_API_KEY,
-                LANGUAGE_MODEL_GENERATE_NAME :process.env.LANGUAGE_MODEL_GENERATE_NAME,
-                LANGUAGE_MODEL_CHAT_NAME: process.env.LANGUAGE_MODEL_CHAT_NAME    
+                LANGUAGE_MODEL_API_KEY : process.env.CHIPPRAGI_LANGUAGE_MODEL_API_KEY,
+                LANGUAGE_MODEL_GENERATE_NAME :process.env.CHIPPRAGI_LANGUAGE_MODEL_GENERATE_NAME,
+                LANGUAGE_MODEL_CHAT_NAME: process.env.CHIPPRAGI_LANGUAGE_MODEL_CHAT_NAME    
         }
     }
     });
     describe("Class testing", function() {
         it("should allow new LangModel to be created", function() {
-            let test = new OpenAIAPI(testConfig);
+            let test = new OpenAIApi(testConfig);
             //validate that test created a new system
             expect(test).to.not.be.undefined;
         });
